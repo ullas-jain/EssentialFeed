dcb3d33ecf78f17369960d602afc95d18cd76464
ullas-jain <jainullas1994@gmail.com>
2023-04-01 18:10:32 +0530

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/FeedItem.swift
+++ EssentialFeed/FeedItem.swift
@@ -8,8 +8,24 @@
 import Foundation
 
 public struct FeedItem: Equatable {
-    let id: UUID
-    let imageURL: URL
-    let description: String?
-    let location: String?
+    public let id: UUID
+    public let imageURL: URL
+    public let description: String?
+    public let location: String?
+    
+    public init(id: UUID, imageURL: URL, description: String?, location: String?) {
+        self.id = id
+        self.imageURL = imageURL
+        self.description = description
+        self.location = location
+    }
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -75,6 +75,37 @@ class RemoteFeedLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_deliversItemsOn200HTTPResponseWithValidJSONList() {
+        let (sut, client) = makeSUT()
+        
+        let item1 = FeedItem(id: UUID(),
+                             imageURL: URL(string: "http://a-url.com")!,
+                             description: nil,
+                             location: nil)
+        let item1JSON = [
+            "id": item1.id.uuidString,
+            "image": item1.imageURL.absoluteString
+        ]
+        let item2 = FeedItem(id: UUID(),
+                             imageURL: URL(string: "http://a-url.com")!,
+                             description: "a description",
+                             location: "a location")
+        let item2JSON = [
+            "id": item2.id.uuidString,
+            "description": item2.description,
+            "location": item2.location,
+            "image": item2.imageURL.absoluteString
+        ]
+        let itemsJSON = [
+            "items": [item1JSON, item2JSON]
+        ]
+        
+        expect(sut, toCompleteWith: .success([item1, item2]), when: {
+            let json = try! JSONSerialization.data(withJSONObject: itemsJSON)
+            client.complete(withStatusCode: 200, data: json)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
