3e3468741c827cd2e103c88bb7c9d6ba365eab3a
ullas-jain <jainullas1994@gmail.com>
2023-04-01 17:27:20 +0530

    Replace Data init with the latest method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -68,7 +68,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
         
-        let invalidJSON = Data(bytes: "invalid json".utf8)
+        let invalidJSON = Data.init(bytes: "invalid json".utf8)
         client.complete(withStatusCode: 200, data: invalidJSON)
         
         XCTAssertEqual(capturedErrors, [.invalidData])
