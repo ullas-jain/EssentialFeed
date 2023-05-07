3b70f1b7067b2dae5579f6277bb434057fe9d7d5
ullas-jain <jainullas1994@gmail.com>
2023-05-07 12:10:58 +0530

    Adds datamodel classes and impln

---
+++ EssentialFeed/CoreDataFeedStore.swift
@@ -0,0 +1,115 @@
+import CoreData
+
+public final class CoreDataFeedStore: FeedStore {
+    public static let modelName = "FeedStore"
+    public static let model = NSManagedObjectModel(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
+
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+
+    public struct ModelNotFound: Error {
+        public let modelName: String
+    }
+
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataFeedStore.model else {
+            throw ModelNotFound(modelName: CoreDataFeedStore.modelName)
+        }
+
+        container = try NSPersistentContainer.load(
+            name: CoreDataFeedStore.modelName,
+            model: model,
+            url: storeURL
+        )
+        context = container.newBackgroundContext()
+    }
+
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
+
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        perform { context in
+            do {
+                if let cache = try ManagedCache.find(in: context) {
+                    let localFeed = try cache.feed.asLocal
+                    completion(.found(feed: localFeed, timestamp: cache.timestamp))
+                } else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        perform { context in
+            do {
+                try ManagedCache.find(in: context)
+                    .map(context.delete)
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = try feed.toBeStored(in: context)
+                try context.save()
+                completion(nil)
+            } catch {
+                context.rollback()
+                completion(error)
+            }
+        }
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        perform { context in
+            do {
+                try ManagedCache.find(in: context)
+                    .map(context.delete)
+                    .map(context.save)
+                completion(nil)
+            } catch {
+                context.rollback()
+                completion(error)
+            }
+        }
+    }
+
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform {
+            action(context)
+        }
+    }
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toBeStored(in context: NSManagedObjectContext) throws -> NSOrderedSet {
+        let managedImages = map { local in
+            let image = ManagedFeedImage(context: context)
+            image.id = local.id
+            image.imageDescription = local.description
+            image.location = local.location
+            image.url = local.url
+            return image
+        }
+        return NSOrderedSet(array: managedImages)
+    }
+}
+
+private extension NSOrderedSet {
+    var asLocal: [LocalFeedImage] {
+        get throws {
+            array.compactMap { stored in
+                guard let stored = stored as? ManagedFeedImage else { return nil }
+                return LocalFeedImage(id: stored.id, url: stored.url, description: stored.imageDescription, location: stored.location)
+            }
+        }
+    }
+}
---
+++ EssentialFeed/ManagedFeedImage.swift
@@ -0,0 +1,10 @@
+import Foundation
+import CoreData
+
+final class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
