f73d7a12941dcea585f0a88fe0763cfcefd65d69
ullas-jain <jainullas1994@gmail.com>
2023-05-14 17:27:48 +0530

    Setup FeedViewControllerTests

---
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+import EssentialFeed
+
+class FeedViewControllerTests: XCTestCase {
+    
+    func test_load_deliversNoItemsOnEmptyCache() {
+        XCTAssertTrue(1 + 1 == 2)
+    }
+}
