27f0e22557d74ec6e4c9b036a86329311806dff1
ullas-jain <jainullas1994@gmail.com>
2023-05-16 09:23:04 +0530

    Move FeedLoader loading state management to FeedViewModel. Now, t…
    …he `FeedRefreshViewController` acts as a binder between the `View` and the `ViewModel`

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift .swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift .swift
@@ -5,9 +5,10 @@ public final class FeedUIComposer {
     private init() {}
 
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
+        let viewModel = FeedViewModel(feedLoader: feedLoader)
+        let refreshController = FeedRefreshViewController(viewModel: viewModel)
         let feedController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+        viewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
         return feedController
     }
     
