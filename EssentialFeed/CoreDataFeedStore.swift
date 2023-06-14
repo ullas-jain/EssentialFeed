e19e6929e46117d10574d3fcee9a5c9aa75806f3
ullas-jain <jainullas1994@gmail.com>
2023-06-14 10:42:41 +0530

    Move CoreDataFeedStore implementation of the FeedStore protocol t…
    …o separate file

--- EssentialFeed/CoreDataFeedStore.swift
+++ EssentialFeed/CoreDataFeedStore.swift
@@ -1,6 +1,6 @@
 import CoreData
 
-public final class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore {
     public static let modelName = "FeedStore"
     
     private let container: NSPersistentContainer
@@ -31,35 +31,6 @@ public final class CoreDataFeedStore: FeedStore {
         context.performAndWait {
             let coordinator = self.container.persistentStoreCoordinator
             try? coordinator.persistentStores.forEach(coordinator.remove)
-        }
-    }
-    
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache.find(in: context).map {
-                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-                }
-            })
-        }
-    }
-    
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
-            completion(Result {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                try context.save()
-            })
-        }
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
-            })
         }
     }
     
